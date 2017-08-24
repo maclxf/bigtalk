1f8c84c60cffa941c889b9fe055706889efd9abc
lixiaofeng <46905313@qq.com>
2017-08-24 09:38:22 +0800

    命名空间演示

--- name_space/Demo.php
+++ name_space/Demo.php
@@ -1,10 +1,26 @@
 <?php
 // 定义一个叫NameSpaceDemo的命名空间
-//namespace NameSpaceDemo;
+namespace NameSpaceDemo;
 
 class Demo {
 
     public function __construct() {
-        echo '__NAMESPACE__ is ' . __NAMESPACE__;
+        echo 'Demo __NAMESPACE__ is ' . __NAMESPACE__ . '<br>';
+    }
+}
+
+
+// 另一个命名空间
+namespace NameSpaceDemo1\BBS;
+// NameSpaceDemo1中定义的变量
+const constDefine = '我是外层的constDefine';
+
+class Demo1 {
+    const constDefine = '我是内层的constDefine';
+
+    public function __construct() {
+        echo 'Demo1 __NAMESPACE__ is ' . __NAMESPACE__ . '<br>';
+        echo '外层const的访问直接用本身定义的变量constDefine：' . constDefine . '<br>';
+        echo '内层const的访问要用self::constDefine：' . self::constDefine;
     }
 }
--- test_namespace.php
+++ test_namespace.php
@@ -1,6 +1,30 @@
 <?php
 require_once 'name_space/Demo.php';
 // 调用命名空间NameSpaceDemo中的Demo
-//use NameSpaceDemo\Demo; //若支注释掉这个不注释掉demo.php中的namespace也不会得到正确的值，因为demo归纳到namespace的命名空间当中
+use NameSpaceDemo\Demo; //若支注释掉这个不注释掉demo.php中的namespace也不会得到正确的值，因为demo归纳到namespace的命名空间当中
 
 new Demo();
+
+
+
+
+echo '<HR>';
+
+
+// 导入一个命名空间
+use NameSpaceDemo1\BBS;
+//如果是子空间还应把子空间的名带上，作为限定名称
+new BBS\Demo1();
+echo '<br>在外层非class的内层访问用NameSpaceDemo1\constDefine：' . BBS\constDefine;
+
+
+
+echo '<HR>';
+
+// 导入一个类并使用别名
+use NameSpaceDemo1\BBS\Demo1 as test;
+//use NameSpaceDemo1\constDefine as testconst; 常亮不能像对象一样用别名
+//Notice: Use of undefined constant testconst - assumed 'testconst' in E:\project\bigtalk\test_namespace.php on line 12
+new test();
+
+echo '<br>在外层非class的内层访问用NameSpaceDemo1\constDefine：' . NameSpaceDemo1\BBS\constDefine;
