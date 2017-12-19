043c56863f58d83e43f603555d00fb8ffd4f2f7b
lxf <46905313@qq.com>
2017-12-19 09:24:58 +0800

    添加依赖注入的说明

--- save_the_world/Container.php
+++ save_the_world/Container.php
@@ -9,8 +9,13 @@ class Container
      * 注册超能力
      * @author lxf 2017-12-14
      * @param  [type] $abstract 注册超能力的名称
-     * @param  [type] $concrete [description]
+     * @param  [type] $concrete 如果是匿名函数那么绑定给binds,否则直接当作是实例给instances
      * @return [type]           [description]
+        $concrete = function() {
+            return new XXX();
+        }
+        $concrete = new XXX();
+        $concrete 有这两种情况
      */
     public function bind($abstract, $concrete)
     {
