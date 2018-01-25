94220b8484602adba0d128d75dcf977178ca39d9
lxf <46905313@qq.com>
2018-01-25 19:19:28 +0800

    测试guzzle

--- GuzzleTest.php
+++ GuzzleTest.php
@@ -3,5 +3,20 @@ require_once __DIR__ . '/vendor/autoload.php';
 use GuzzleHttp\Client;
 
 $client = new \GuzzleHttp\Client();
-$res = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
-echo $res->getStatusCode();
+$order_no = '810561975159';
+//$res = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
+//echo $res->getStatusCode();
+$arr = [
+    'query' => [
+        'key_id' => 'kuai',
+        'secret_key' => '123456789',
+        'order_no' => $order_no,
+        'order_by' => 'DESC'
+    ],
+
+];
+
+$res = $client->request('GET', 'http://adi-fr.dev/api', $arr);
+
+echo '<pre>';
+var_dump($res);
--- LiftStatusTest.php
+++ LiftStatusTest.php
@@ -8,11 +8,14 @@ use liftstatustest\ClosingStatus;
 use liftstatustest\RunningStatus;
 use liftstatustest\StoppingStatus;
 
-
+// 默认的上下文类
 $context = new Context();
+// 设置上下文类持有关闭状态
 $context->setLiftStatus(new ClosingStatus());
 
 $context->open();
 $context->close();
 $context->run();
 $context->stop();
+
+// context与LiftStatus下的多个实际状态实例是聚合关系context owns LiftStatus(openning....),聚合具体表现为整体和部分的关系，部分离开整体后还是可以单独存在
