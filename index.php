76719648f5734407f593618ff41a9b74b7ef36c7
maclxf <46905313@qq.com>
2021-12-27 11:07:07 +0800

    修改入口文件

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+        const p = new Promise((resolve, reject) => {
+            resolve('成功结果')
+        });
+
+        const t = p.then((value) => {
+            console.log('成功调用1', value)
+            return kkkk;
+        }, (reson) => {
+            console.log('失败调用1', reson)
+        });
+
+        t.then((value) => {
+            console.log('成功调用2', value)
+        }, (error) => {
+            console.log('失败调用2', error)
+        });
+
+        console.log(t)
+    </script>
+</head>
+<body>
 <?php
-echo '我是大话设计模式练习项目';
+    echo '我是大话设计模式练习项目';
 ?>
+
+</body>
+</html>
